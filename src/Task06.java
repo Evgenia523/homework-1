0504de7e561db4515ee2f6c35dcba95d706b9041
Evgenia523 <evgenia523@mail.ru>
2024-04-18 20:48:03 +0400

    add Task06.java

---
+++ src/Task06.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Task06 {
+    public static void main(String[] args) {
+        final int W = 5;
+        final int H = 10;
+        System.out.println("Задание 6");
+        int s = W * H;
+        int p = (W+H) * 2;
+        System.out.println("Площадь: " + s);
+        System.out.println("Периметр: " + p);
+    }
+}
